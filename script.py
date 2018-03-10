10c15858febecac8c0b0f60472b10f3f04408e62
Max <jack.moriarty@mail.ru>
2018-03-10 23:30:27 +0700

    Added documentation for  script.py

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+#this script does make list of number of integers you enter and finds its sum
+
 L = [int(input('enter ['+str(i)+'] element:')) for i in range(int(input('ENter the number of elements:')))]
 print('Amount of elements in L - ',len(L))
 print('Sum of L = ',sum(L))
