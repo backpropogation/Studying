24e83e1c09c1ee8783c4df799e09c88d8e91e79e
Max <jack.moriarty@mail.ru>
2018-03-10 23:26:35 +0700

    Added script.py

---
+++ script.py
@@ -0,0 +1,5 @@
+L = [int(input('enter ['+str(i)+'] element:')) for i in range(int(input('ENter the number of elements:')))]
+print('Amount of elements in L - ',len(L))
+print('Sum of L = ',sum(L))
+print(L)
+
