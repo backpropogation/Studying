5b2fb365fd6bc775d29effbf9478ae2b91d5fc20
Max <jack.moriarty@mail.ru>
2018-03-12 19:24:42 +0700

    Added listCreator func

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 #this script does make list of number of integers you enter and finds its sum
+def listCreator():
+    L = [int(input('enter ['+str(i)+'] element:')) for i in range(int(input("ENter the number of elements:")))]
+    return L
 
-L = [int(input('enter ['+str(i)+'] element:')) for i in range(int(input('ENter the number of elements:')))]
+L = listCreator()
 print('Amount of elements in L - ',len(L))
 print('Sum of L = ',sum(L))
 print(L)
+
 
